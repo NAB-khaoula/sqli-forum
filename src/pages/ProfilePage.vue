e8bca419f6145483d468b97a9c06ee8c91b3593e
khaoula NABOUSSI <k12.naboussi@gmail.com>
2022-11-22 09:39:37 +0100

    add user profile card component

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -2,33 +2,9 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-              :src="user.avatar"
-              :alt="`${user.name} profile picture`"
-            >
-          </p>
-          <h1 class="title">
-            {{ user.username }}
-          </h1>
-          <p class="text-lead">
-            {{ user.name }}
-          </p>
-          <p class="text-justify">
-            {{ user.bio || "no specified user bio" }}
-          </p>
-          <span class="online">{{ user.username }} is online</span>
-          <div class="stats">
-            <span>{{ userPostsCount }}</span>
-            <span>{{ userThreadsCount }}</span>
-          </div>
-          <hr>
-          <p class="text-large text-center">
-            <i class="fa fa-globe" />
-            <a :href="user.website">{{ user.website }}</a>
-          </p>
-        </div>
+        <user-profile-card
+          :user="user"
+        />
         <p class="text-xsmall text-faded text-center">
           Member since june ...
         </p>
@@ -54,30 +30,16 @@
 
 <script>
 import PostListView from "../components/PostListView";
+import UserProfileCard from "@/components/UserProfileCard.vue";
 import { mapGetters } from "vuex";
 
 export default {
   components: {
     PostListView,
+    UserProfileCard,
   },
   computed: {
     ...mapGetters({ user: "authUser" }),
-    userPosts() {
-      return Object.values(this.$store.state.posts).filter(
-        (post) => post.userId === this.user.id
-      );
-    },
-    userPostsCount() {
-      return this.userPosts.length;
-    },
-    userThread() {
-      return Object.values(this.$store.state.threads).filter(
-        (thread) => thread.userId === this.user.id
-      );
-    },
-    userThreadsCount() {
-      return this.userThread.length;
-    },
   },
 };
 </script>
